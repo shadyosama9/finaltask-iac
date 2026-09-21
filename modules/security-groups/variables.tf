98eac7fd08146eaa3b26ed8f113509caf95dcd34
Shady Osama <shadyosama554@gmail.com>
2026-09-21 10:59:55 +0300

    making tags optional

--- modules/security-groups/variables.tf
+++ modules/security-groups/variables.tf
@@ -15,26 +15,28 @@ variable "security_groups" {
     name        = string
     description = string
     vpc_key     = string
-    tags        = map(any)
+    tags = optional(map(any), {})
+
     ingress_rules = optional(list(object({
-      name                        = string
-      ip_protocol                 = string
-      from_port                   = optional(number)
-      to_port                     = optional(number)
-      cidr_ipv4                   = optional(string)
-      referenced_security_group_id = optional(string)
+      name                          = string
+      ip_protocol                   = string
+      from_port                     = optional(number)
+      to_port                       = optional(number)
+      cidr_ipv4                     = optional(string)
+      referenced_security_group_id  = optional(string)
       referenced_security_group_key = optional(string)
-      tags                        = optional(map(any), {})
+      tags                          = optional(map(any), {})
     })), [])
+    
     egress_rules = optional(list(object({
-      name                        = string
-      ip_protocol                 = string
-      from_port                   = optional(number)
-      to_port                     = optional(number)
-      cidr_ipv4                   = optional(string)
-      referenced_security_group_id = optional(string)
+      name                          = string
+      ip_protocol                   = string
+      from_port                     = optional(number)
+      to_port                       = optional(number)
+      cidr_ipv4                     = optional(string)
+      referenced_security_group_id  = optional(string)
       referenced_security_group_key = optional(string)
-      tags                        = optional(map(any), {})
+      tags                          = optional(map(any), {})
     })), [])
   }))
 }
